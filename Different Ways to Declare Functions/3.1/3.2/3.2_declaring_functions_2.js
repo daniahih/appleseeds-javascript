2650a7c01f0e9aa4ff74ef1d33e42775740b4b88
daniahih <daniahih310@gmail.com>
2023-02-13 14:10:49 +0200

    added 4.1

---
+++ Different Ways to Declare Functions/3.1/3.2/3.2_declaring_functions_2.js
@@ -0,0 +1,14 @@
+/*
+JavaScript - Declaring Functions
+The following exercise contains the following
+subjects:
+* functions
+Instructions
+* Please reformat the following function
+expressions to IIFE functions.
+* Please reformat the following function
+declarations in two ways, explicit return and implicit
+return functions.
+*/
+
+
