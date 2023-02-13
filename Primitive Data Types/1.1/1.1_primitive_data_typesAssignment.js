e036b16cf3f7d4a8a676210a708566072ee80818
daniahih <daniahih310@gmail.com>
2023-02-13 10:37:03 +0200

    added primative data type

---
+++ Primitive Data Types/1.1/1.1_primitive_data_typesAssignment.js
@@ -0,0 +1,28 @@
+// 1. Create a variable whose value holds a string.
+// 2. Create a variable whose value holds a number.
+// 3. Create a variable whose value holds an object.
+// 4. Create a variable whose value holds a boolean.
+// 5. Create a variable whose value holds null.
+// 6. Create a variable whose value is undefined.
+// 7. Create a variable whose value holds a symbol.
+// Console log the types of your variables.
+
+ const name="daniahih"; //1
+ const age=24;//2
+ const obj={
+    name:'dania',
+    age:24,
+    university: 'alquds university'
+
+
+ }//3
+ const log=true;//4
+ const value = null //5 
+const value2=undefined//6
+console.log(name);
+console.log(age);
+console.log(obj);
+console.log(log)
+console.log(value)
+console.log(value2)
+
