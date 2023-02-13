b71b0652b38af19550ecfca1e7aeb2027cd47e8b
daniahih <daniahih310@gmail.com>
2023-02-13 10:42:50 +0200

    added 2-1 function

---
+++ Function Basics/2.1_functions.js
@@ -0,0 +1,6 @@
+function countryInfo(country,population,capitalCity){
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+console.log(countryInfo("palestine",9,"jerusalem"))
+console.log(countryInfo('china',90,"tokyo"))
+console.log(countryInfo("jordan",15,'amman'))
