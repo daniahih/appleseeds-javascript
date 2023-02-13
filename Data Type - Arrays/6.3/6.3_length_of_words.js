b3b7a9a408760750c3f554fe154934e1bdee6f2a
daniahih <daniahih310@gmail.com>
2023-02-13 20:22:00 +0200

    added 6.4

---
+++ Data Type - Arrays/6.3/6.3_length_of_words.js
@@ -0,0 +1,13 @@
+// Write a function that will receive an array of strings and will
+// return a new array containing all the lengths of all the strings in
+// the array it got as a parameter.
+// So that if you call this function with this array for example:
+// ["boo", "doooo", "hoo","ro"]
+// it will return this array: [3, 5, 3, 2].
+
+function countLenght(array){
+    for(let i=0 ;i<array.length;i++){
+        console.log(i,array[i].length)
+    }
+}
+countLenght(["dania","mohammad","hamza","hih"])
