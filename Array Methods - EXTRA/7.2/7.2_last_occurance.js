902fd96adf76c6b5906242e2c7af23f430db701a
daniahih <daniahih310@gmail.com>
2023-02-13 21:43:55 +0200

    added 7.2

---
+++ Array Methods - EXTRA/7.2/7.2_last_occurance.js
@@ -0,0 +1,7 @@
+
+// We want to get the index of the last occurrence of the word
+// “you”. It should give you 42.
+const string = "The more you know, the more you know that you don't know";
+console.log(string.lastIndexOf("you"))
+
+console.log(string.indexOf("you",30))
